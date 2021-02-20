bd187722c433a759ce3e0be49c28cefde21d7552
gerwini <gerwinjb@hotmail.nl>
2021-02-20 20:57:57 +0000

    Profile page completed and Admin product editing and deleteing added. Need to make it so if you click on image it opens in a new tab

--- products/urls.py
+++ products/urls.py
@@ -4,8 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.all_products, name='store'),
     path('purchase', views.view_purchase, name='view_purchase'),
-    path('<product_id>', views.product_info, name='product_info'),
+    path('<int:product_id>', views.product_info, name='product_info'),
     path('add/<item_id>', views.add_for_purchase, name='add_for_purchase'),
     path('modify/<item_id>', views.modify_purchase, name='modify_purchase'),
     path('remove/<item_id>', views.remove_product, name='remove_product'),
+    path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
