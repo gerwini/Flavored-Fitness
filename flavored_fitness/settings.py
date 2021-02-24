f4036f746977ebe56db923fdce02cb256911e105
gerwini <gerwinjb@hotmail.nl>
2021-02-24 20:51:08 +0000

    removed secret key

--- flavored_fitness/settings.py
+++ flavored_fitness/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*kcdv8$v9hrf)o9aww0_1a8a$+4o&3#ty(an77)ogr=97$15fz'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['flavored-fitness.herokuapp.com', 'localhost']
 
