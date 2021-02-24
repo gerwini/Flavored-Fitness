ccf188f91ce60b3cdf583e2a0db7a4fda13572d1
gerwini <gerwinjb@hotmail.nl>
2021-02-24 20:23:07 +0000

    Set up emails to be actually send

--- flavored_fitness/settings.py
+++ flavored_fitness/settings.py
@@ -185,4 +185,15 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 FREE_DELIVERY_THRESHOLD = 10
 STANDARD_DELIVERY_PERCENTAGE = 10
-DEFAULT_FROM_EMAIL = 'flavoredfitness@example.com'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'flavoredfitness@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
